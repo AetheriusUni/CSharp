de25c5b5c67e4d98f178f40b492e8d199e1788a6
Marc Raymond A. Serrano <marc.serrano@ca.rr.com>
2017-06-28 19:47:47 -0700

    Odd or Even

---
+++ MarcSerranoExercise3/Exercise3/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int integer1;
+            Console.Write("Input your integer: ");
+            integer1 = Convert.ToInt32(Console.ReadLine());
+            if (integer1 % 2 == 0)
+                Console.WriteLine("Your integer is Even.");
+            else
+                Console.WriteLine("Your integer is Odd.");
+
+        }
+    }
+}
+/*Input your integer: 456
+Your integer is Even.
+Press any key to continue . . .
+ */
+/*Input your integer: 321
+Your integer is Odd.
+Press any key to continue . . .
+*/
+//The program still works as intended with the use of double.
+/*Double Program
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double integer1;
+            Console.Write("Input your integer: ");
+            integer1 = Convert.ToInt32(Console.ReadLine());
+            if (integer1 % 2 == 0)
+                Console.WriteLine("Your integer is Even.");
+            else
+                Console.WriteLine("Your integer is Odd.");
+
+        }
+    }
+}
+*/
