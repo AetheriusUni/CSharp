ead9697bd9c72ef0da9b47828050bf12a7cd6abc
Marc Raymond A. Serrano <marc.serrano@ca.rr.com>
2017-07-01 01:35:14 -0700

    Simple Object-Oriented Programming

---
+++ MarcSerranoExercise8/MarcSerranoExercise8/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarcSerranoExercise8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Temperature room = new Temperature();
+            room.GetData();
+            room.DisplayDegree();
+        }
+    }
+}
---
+++ MarcSerranoExercise8/MarcSerranoExercise8/temperature.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace MarcSerranoExercise8
+{
+    class Temperature
+    {
+        char scale;
+        double degree;
+        public void GetData()
+        {
+            Console.Write("\n Please enter the room temprature:");
+            degree = Convert.ToDouble(Console.ReadLine());
+            Console.Write("\n Please enter the scale you used for room temperature:");
+            scale = Convert.ToChar(Console.Read());
+        }
+        public void DisplayDegree()
+        {
+            Console.WriteLine("\n The room temperature is {0} {1}\n", degree, scale);
+        }
+    }
+}
+
+/*
+ * UML = Uniform Modeling Language
+Temperature
+char scale; double degree;
+void GetData(); void DisplayDegree();
+*/
+
+/*
+if we change the data members to be public, we can:
+Temperature room = new Temperature();
+room.GetData();
+Console.WriteLine("\n The room temperature is {0} {1}\n", degree, scale);
+*/
